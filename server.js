3a18ffa69428955b891ecd74736fc1376923b5f2
Crystal Danger <cdneal091@gmail.com>
2020-02-16 14:07:54 -0500

    serves static images

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const fs = require('fs')
 const app = express()
 const port = 3001
 
+// checks to see if file is available and file type, send it if it is or return a 404 if not
+app.get('/media/:fileID', (req, res) => {
+  fs.readdir(`/media/${req.params.fileID}/`, (err, file) => {
+    console.log(err);
+    console.log(file);
+    res.sendFile(__dirname + `/media/${req.params.fileID}/${req.params.fileID}`)
+  })
+})
 
+// check to see if thumb is available, send it if so
+// no need to check file extension, all thumbs are .jpg
 app.get('/thumb/:fileID', (req, res) => {
   console.log(res);
   res.send('okie')
 })
 
-
-app.get('/media/:fileID', (req, res) => {
-  res.sendFile(__dirname + `/media/${req.params.fileID}/${req.params.fileID}`)
-})
 
 app.post('/media', (req, res) => {
 
