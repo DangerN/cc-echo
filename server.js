d77cb3abbbbbac17351a92c3a1ba0f015174700e
Crystal Danger <cdneal091@gmail.com>
2020-02-16 15:34:19 -0500

    it can now upload files

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const express = require('express')
 const fs = require('fs')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3001
 
-// checks to see if file is available and file type, send it if it is or return a 404 if not
+app.use(bodyParser.raw({type: 'application/octet-stream', limit: '4mb'}))
+
+// check to see if file is available and send file or 404
 app.get('/media/:fileID', (req, res) => {
-  fs.readdir(`/media/${req.params.fileID}/`, (err, file) => {
-    console.log(err);
-    console.log(file);
-    res.sendFile(__dirname + `/media/${req.params.fileID}/${req.params.fileID}`)
+  fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
+    err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[0]}`)
   })
 })
 
-// check to see if thumb is available, send it if so
-// no need to check file extension, all thumbs are .jpg
+// check to see if file is available and send thumb or 404
 app.get('/thumb/:fileID', (req, res) => {
-  console.log(res);
-  res.send('okie')
+  fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
+    err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[1]}`)
+  })
 })
 
-
+// create thumbnail and save to meida folder then respond with status
+// TODO:
 app.post('/media', (req, res) => {
-
+  let name = req.get('File-Name')
+  let ext = req.get('File-Extension')
+  let buffer = Buffer.from(req.body)
+  fs.mkdir(__dirname + `/media/${name}/`, (err) => {
+    if (err) { throw err }
+    fs.writeFile(__dirname + `/media/${name}/${name}.${ext}`, buffer, (err) => {
+      err ? res.send('no dice') : res.send('seems ok')
+    })
+  })
 })
 
 app.get('*', (req, res) => {
