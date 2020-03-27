c6b4330f324d53eb35e2e71731d26f77610da870
Crystal Danger <cdneal091@gmail.com>
2020-03-27 15:28:31 -0400

    moved routes to controller

---
+++ controllers/main.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path');
+const fileS = require('./util/fileS');
+
+
+router.use(bodyParser.raw({type: 'application/octet-stream', limit: '4mb'}))
+
+// check to see if file is available and send file or 404
+router.get('/media/:fileID', (req, res) => {
+  fs.readdir(path.dirname(__dirname) + `/media/${req.params.fileID}/`, (err, file) => {
+    err ? res.send('404') : res.sendFile(path.dirname(__dirname) + `/media/${req.params.fileID}/${file[1]}`)
+  })
+})
+
+// check to see if file is available and send thumb or 404
+router.get('/thumb/:fileID', (req, res) => {
+  fs.readdir(path.dirname(__dirname) + `/media/${req.params.fileID}/`, (err, file) => {
+    err ? res.send('404') : res.sendFile(path.dirname(__dirname) + `/media/${req.params.fileID}/${file[0]}`)
+  })
+})
+
+
+// create thumbnail and save to meida folder then respond with status
+router.post('/media', (req, res) => {
+  let name = req.get('File-Name')
+  let ext = req.get('File-Extension')
+  let buffer = Buffer.from(req.body)
+  console.log(req.body);
+
+  fs.mkdir(path.dirname(__dirname) + `/media/${name}/`, (err) => {
+    err && util.handleErr(err)
+
+    fs.writeFile(path.dirname(__dirname) + `/media/${name}/${name}.${ext}`, buffer, (err) => {
+      err && util.handleErr(err)
+
+      imageThumbnail(buffer, {height: 128, width: 128})
+      .then(thumb=>{
+        fs.writeFile(path.dirname(__dirname) + `/media/${name}/${name}-thumb.${ext}`, thumb, (err) => {
+          err && util.handleErr(err)
+        })
+      })
+      .catch(err=>console.log(err))
+      res.send('seems ok')
+    })
+
+  })
+})
+
+router.get('*', (req, res) => {
+  res.send('go away')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,57 +1,58 @@
 const express = require('express')
 const fs = require('fs')
-const bodyParser = require('body-parser')
 const imageThumbnail = require('image-thumbnail')
 const util = require('./util')
-
+const main = require('./controllers/main.js');
 const app = express()
 const port = 3001
 
-app.use(bodyParser.raw({type: 'application/octet-stream', limit: '4mb'}))
+app.use('/', main)
 
-// check to see if file is available and send file or 404
-app.get('/media/:fileID', (req, res) => {
-  fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
-    err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[1]}`)
-  })
-})
-
-// check to see if file is available and send thumb or 404
-app.get('/thumb/:fileID', (req, res) => {
-  fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
-    err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[0]}`)
-  })
-})
-
-
-// create thumbnail and save to meida folder then respond with status
-app.post('/media', (req, res) => {
-  let name = req.get('File-Name')
-  let ext = req.get('File-Extension')
-  let buffer = Buffer.from(req.body)
-  console.log(req.body);
-
-  fs.mkdir(__dirname + `/media/${name}/`, (err) => {
-    err && util.handleErr(err)
-
-    fs.writeFile(__dirname + `/media/${name}/${name}.${ext}`, buffer, (err) => {
-      err && util.handleErr(err)
-
-      imageThumbnail(buffer, {height: 128, width: 128})
-      .then(thumb=>{
-        fs.writeFile(__dirname + `/media/${name}/${name}-thumb.${ext}`, thumb, (err) => {
-          err && util.handleErr(err)
-        })
-      })
-      .catch(err=>console.log(err))
-      res.send('seems ok')
-    })
-
-  })
-})
-
-app.get('*', (req, res) => {
-  res.send('go away')
-})
+// app.use(bodyParser.raw({type: 'application/octet-stream', limit: '4mb'}))
+//
+// // check to see if file is available and send file or 404
+// app.get('/media/:fileID', (req, res) => {
+//   fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
+//     err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[1]}`)
+//   })
+// })
+//
+// // check to see if file is available and send thumb or 404
+// app.get('/thumb/:fileID', (req, res) => {
+//   fs.readdir(__dirname + `/media/${req.params.fileID}/`, (err, file) => {
+//     err ? res.send('404') : res.sendFile(__dirname + `/media/${req.params.fileID}/${file[0]}`)
+//   })
+// })
+//
+//
+// // create thumbnail and save to meida folder then respond with status
+// app.post('/media', (req, res) => {
+//   let name = req.get('File-Name')
+//   let ext = req.get('File-Extension')
+//   let buffer = Buffer.from(req.body)
+//   console.log(req.body);
+//
+//   fs.mkdir(__dirname + `/media/${name}/`, (err) => {
+//     err && util.handleErr(err)
+//
+//     fs.writeFile(__dirname + `/media/${name}/${name}.${ext}`, buffer, (err) => {
+//       err && util.handleErr(err)
+//
+//       imageThumbnail(buffer, {height: 128, width: 128})
+//       .then(thumb=>{
+//         fs.writeFile(__dirname + `/media/${name}/${name}-thumb.${ext}`, thumb, (err) => {
+//           err && util.handleErr(err)
+//         })
+//       })
+//       .catch(err=>console.log(err))
+//       res.send('seems ok')
+//     })
+//
+//   })
+// })
+//
+// app.get('*', (req, res) => {
+//   res.send('go away')
+// })
 
 app.listen(port, console.log(`Good Luck from port ${3001}!`))
